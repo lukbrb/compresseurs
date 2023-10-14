fc79e0245e5598c056731c266812bfc9a11f5491
lukbrb <lucasbarbr@outlook.com>
2023-10-14 17:22:03 +0200

    implémentation ex wikipedia

--- zip-comp/naivezip.py
+++ zip-comp/naivezip.py
@@ -2,6 +2,8 @@
 
 import sys
 
+table = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
 def main():
     if len(sys.argv) < 2:
         print("[ERREUR] No file argument were provided. Usage: \n\tnaivezip.py <file-to-compress>\n")
@@ -9,25 +11,40 @@ def main():
         filename = sys.argv[1]
         with open(filename) as f: contenu = f.read()
         compressed = lzw_compress(contenu)
-        print(compressed)
+        print("Compressed message :", compressed)
+        
 
-
-def lzw_compress(content: str) -> list[bytes]:
+# TODO: Améliorer code pour comptabiliser dernière valeur de w
+def lzw_compress(content: str) -> bytearray:
     print("w\t c\t wc\t sortie\t dictionnaire ")
     w = ""
-    # records = list()
+    compressed_message = ""
     dico = dict()  # {c : str(ord(c)) for c in content}
-    init = 254
+    init = 255
+
     for c in content:
         p = w + c
-        if p in dico.keys():
+        if p in dico.keys() or p in table:
+            print(w, "\t", c, "\t", p, "\t", " ", "\t", " ")
             w = p
         else:
             init += 1
             dico[p] = f"<{init}>" # "".join([str(ord(l)) for l in p])
             print(w, "\t", c, "\t", p, "\t", dico.get(w, w), "\t", p + "=" + dico[p])
+            compressed_message +=  dico.get(w, w)
             w = c
-    # print(dico)
+
+    if w in dico.keys() or w in table:
+            print(w, "\t", " ", "\t", " ", "\t", dico.get(w, w), "\t"," ")
+            compressed_message +=  dico.get(w, w)
+    else:
+        init += 1
+        dico[w] = f"<{init}>" # "".join([str(ord(l)) for l in p])
+        print(w, "\t", " ", "\t", " ", "\t", dico.get(w, w), "\t", w + "=" + dico[w])
+        compressed_message +=  dico.get(w, w)
+
+    return compressed_message
+
 
 if __name__ == '__main__':
     main()
